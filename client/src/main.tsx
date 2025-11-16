72656a6f22259915c42fd407c4a82383a18e3118
Yash Shah <yhshah2009@gmail.com>
2025-11-16 20:37:24 +0530

    Fix router base for GitHub Pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Router } from "wouter";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router base="/Thailand2025">
+    <App />
+  </Router>
+);
